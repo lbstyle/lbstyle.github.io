b511071a6e401df9f52824d79e3aa96b499a60ea
lbstyle <35669129+lbstyle@users.noreply.github.com>
2020-05-24 01:48:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var template =`
-<!DOCTYPE>
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <script>
-            alert("hello");
-        </script>
-    </body>
-</html>`;
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
 
-var blob = new Blob([template], { type: "text/html" });
-var blob_url = URL.createObjectURL(blob);
-var blob_a = document.querySelector('#blob-test');
-blob_a.href = blob_url;
+/* Put code here */
+
+
+
+/* Only register a service worker if it's supported */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
+
+/**
+ * Warn the page must be served over HTTPS
+ * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
+ * Installability requires a service worker with a fetch event handler, and
+ * if the page isn't served over HTTPS, the service worker won't load.
+ */
+if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS');
+  const link = requireHTTPS.querySelector('a');
+  link.href = window.location.href.replace('http://', 'https://');
+  requireHTTPS.classList.remove('hidden');
+}
---
+++ service-worker.js
@@ -0,0 +1,18 @@
+
+Report Abuse
+self.addEventListener('install', (event) => {
+  console.log('👷', 'install', event);
+  self.skipWaiting();
+});
+​
+self.addEventListener('activate', (event) => {
+  console.log('👷', 'activate', event);
+  return self.clients.claim();
+});
+​
+self.addEventListener('fetch', function(event) {
+  // console.log('👷', 'fetch', event);
+  event.respondWith(fetch(event.request));
+});
+Avatar for make-it-installablemake-it-installable
+by
